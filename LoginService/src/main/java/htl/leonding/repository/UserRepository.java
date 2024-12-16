a4b96b27c9ed231fdad2c37ae2f162d2f9908d6f
Daniel-Kirchmayr <d.kirchmayr@students.htl-leonding.ac.at>
2024-12-16 10:08:18 +0100

    Added Repository

---
+++ LoginService/src/main/java/htl/leonding/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package htl.leonding.repository;
+
+import htl.leonding.model.User;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserRepository implements PanacheRepositoryBase<User, Long>{
+
+}
